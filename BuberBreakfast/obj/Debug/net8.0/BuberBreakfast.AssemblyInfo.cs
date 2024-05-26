73fd87e20167e213563fe9bcb35927f47d271b4e
Prabesh Narsingh Kunwar <prabeshkunwar12@gmail.com>
2024-05-26 16:57:05 -0300

    initial setup for our app

--- BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
+++ BuberBreakfast/obj/Debug/net8.0/BuberBreakfast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67ed0c3644bab658db813e0ed85766811a9910fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BuberBreakfast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
